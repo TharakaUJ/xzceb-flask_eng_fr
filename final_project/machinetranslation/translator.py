bbe552426e9c56c2dd49bce3b571b9d6e4f5c476
Tharakauj <9dmpires2k17.tuj@gmail.com>
2023-07-08 15:54:57 -0400

    added translation.py

---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,18 @@
+ from deep_translator import MyMemoryTranslator
+ 
+
+def englishToFrench(englishText):
+    '''
+    docstring for this function
+    '''
+
+    frenchText = MyMemoryTranslator(source='en', target='fr').translate(text=englishText)
+    return frenchText
+
+
+def frenchToEnglish(frenchText):
+    '''
+    docstring forthe function
+    '''
+    englishText = MyMemoryTranslator(source='fr', target='en').translate(text=englishText)
+    return englishText
